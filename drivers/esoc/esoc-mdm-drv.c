DD3Boh/android_kernel_nubia_sm8250

drivers: esoc: mdm: Set boot fail action to NOP
 * This is useful since it should allow preventing a kernel panic
   and a full system restart when the modem fails to bootup

Change-Id: I5f653c15e6f31b1a96cf4f9c8c17089289c82505

drivers/esoc/esoc-mdm-drv.c
@@ -17,7 +17,11 @@
 
 #define ESOC_MAX_PON_TRIES	5
 
+#ifdef CONFIG_MACH_NUBIA_NX659J
+#define BOOT_FAIL_ACTION_DEF BOOT_FAIL_ACTION_NOP
+#else
 #define BOOT_FAIL_ACTION_DEF BOOT_FAIL_ACTION_PANIC
+#endif
 
 enum esoc_pon_state {
 	PON_INIT,

innocentjay/Munch

drivers/esoc/esoc-mdm-drv.c
@@ -17,7 +17,11 @@
 
 #define ESOC_MAX_PON_TRIES	5
 
+#ifdef CONFIG_MACH_XIAOMI_SM8250
+#define BOOT_FAIL_ACTION_DEF BOOT_FAIL_ACTION_NOP
+#else
 #define BOOT_FAIL_ACTION_DEF BOOT_FAIL_ACTION_PANIC
+#endif
 
 enum esoc_pon_state {
 	PON_INIT,